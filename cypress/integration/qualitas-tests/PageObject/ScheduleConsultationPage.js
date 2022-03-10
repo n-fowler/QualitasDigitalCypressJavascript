11109621b2d6ea5c2087732b79ab1319632f7b21
n-fowler <45500369+n-fowler@users.noreply.github.com>
2022-03-09 19:07:33 -0500

    add Schedule Consultation tests

---
+++ cypress/integration/qualitas-tests/PageObject/ScheduleConsultationPage.js
@@ -0,0 +1,41 @@
+import ScheduleConsultationPageLocators from "../Locators/ScheduleConsultationPageLocators"
+
+class ScheduleConsultationPage {
+    navigate() {
+        cy.visit(ScheduleConsultationPageLocators.url())
+    }
+
+    getLogoImage() {
+        return cy.get(ScheduleConsultationPageLocators.logo_image())
+    }
+
+    getHeaderContent() {
+        return cy.xpath(ScheduleConsultationPageLocators.header_content())
+    }
+
+    getLeftContent() {
+        return cy.xpath(ScheduleConsultationPageLocators.left_content())
+    }
+
+    getOurCommitmentLink() {
+        return cy.xpath(ScheduleConsultationPageLocators.our_commitment_link())
+    }
+
+    getFaqLink() {
+        return cy.xpath(ScheduleConsultationPageLocators.faq_link())
+    }
+
+    getTermsAndConditionsLink() {
+        return cy.xpath(ScheduleConsultationPageLocators.terms_and_conditions_link())
+    }
+
+    getScheduleConsultationButton() {
+        return cy.get(ScheduleConsultationPageLocators.schedule_consultation_button())
+    }
+
+    getScheduleConsultationButtonLink() {
+        return this.getScheduleConsultationButton().find(ScheduleConsultationPageLocators.schedule_consultation_button_link())
+    }
+}
+
+export default ScheduleConsultationPage
--- cypress/integration/qualitas-tests/schedule_consultation_spec.js
+++ cypress/integration/qualitas-tests/schedule_consultation_spec.js
@@ -1,7 +1,42 @@
-describe('Qualitas - Schedule Consultation - Load', () => {
+import ScheduleConsultationPage from "./PageObject/ScheduleConsultationPage"
+
+  describe('Qualitas - Schedule Consultation - Load', () => {
     it('The Schedule Consultation Page Loads', () => {
-      cy.visit('https://www.qualitasdigital.com/schedule-consultation')
-      cy.get('#header > div.logo-wrapper.flex-item > div')
-        .should('have.class', 'logo-image')
+      const scheduleConsultationPage = new ScheduleConsultationPage();
+      scheduleConsultationPage.navigate();
+      scheduleConsultationPage.getLogoImage().should('have.class', 'logo-image')
+    });
+  });
+
+  describe('Qualitas - Schedule Consultation - Content', () => {
+    before(function() {
+      cy.fixture('ScheduleConsultationPageTestData').then(function (testdata) {
+        this.testdata = testdata
+      });
+    });
+
+    it('The Schedule Consultation Page Content Is Present', function() {
+      const scheduleConsultationPage = new ScheduleConsultationPage();
+      scheduleConsultationPage.navigate();
+
+      //Validate Title
+      scheduleConsultationPage.getHeaderContent().should('have.text', this.testdata.header_content)
+
+      //Validate left section
+      scheduleConsultationPage.getLeftContent().should('have.text', this.testdata.left_content)
+      scheduleConsultationPage.getOurCommitmentLink().invoke('prop', 'href').then((actualHref) => {
+        expect(actualHref).to.equal(this.testdata.our_commitment_link)
+      });
+      scheduleConsultationPage.getFaqLink().invoke('prop', 'href').then((actualHref) => {
+        expect(actualHref).to.equal(this.testdata.faq_link)
+      });
+      scheduleConsultationPage.getTermsAndConditionsLink().invoke('prop', 'href').then((actualHref) => {
+        expect(actualHref).to.equal(this.testdata.terms_and_conditions_link)
+      });
+
+      //Validate right section
+      scheduleConsultationPage.getScheduleConsultationButtonLink().invoke('prop', 'href').then((actualHref) => {
+        expect(actualHref).to.equal(this.testdata.schedule_consultation_button_link)
+      });
     })
-  })
+  });
